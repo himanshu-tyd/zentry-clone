e48a7d7de0605f623c19de420597f106c51e039a
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-12-17 15:17:03 +0530

    task: hero and about is done

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface buttonProps {
+  id: string;
+  title: string;
+  rightIcon?: React.ReactElement;
+  leftIcon?: React.ReactElement;
+  containerClass?: string;
+}
+
+const Button = ({
+  title,
+  id,
+  rightIcon,
+  leftIcon,
+  containerClass,
+}: buttonProps) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass} `}
+    >
+      <span className="relative incline-flex overlfow-hidden font-general text-xs uppercase ">
+        <div>{title}</div>
+      </span>
+      <span>{leftIcon}</span>
+    </button>
+  );
+};
+
+export default Button;
