ef2f7e857b8a6710a89b37801985732de08a5f14
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-12-18 20:11:29 +0530

    fix: radiant title

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -101,7 +101,7 @@ const Features = () => {
             src="videos/feature-1.mp4"
             title={
               <>
-                radi<b>b</b>t
+                radia<b>n</b>t
               </>
             }
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
